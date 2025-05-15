bb10c7b39b66b8907600a1aeaf998a61954047e7
Justt-Mel <justinmelendezj@gmail.com>
2025-05-15 12:24:26 -0400

    added A Home Page Component, and added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { useState } from "react"
+import { Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
 
 function App() {
   const [token, setToken] = useState(null)
 
   return (
-    <div>
-      <h1><img id='logo-image'/>Library App</h1>
-
-      <p>Complete the React components needed to allow users to browse a library catalog, check out books, review their account, and return books that they've finished reading.</p>
-
-      <p>You may need to use the `token` in this top-level component in other components that need to know if a user has logged in or not.</p>
-
-      <p>Don't forget to set up React Router to navigate between the different views of your single page application!</p>
-    </div>
+    <Routes>
+      <Route path ="/" element={<Home/>}/>
+    </Routes>
   )
 }
 
