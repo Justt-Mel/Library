41d8b26c3e4a0657f7b7895fca2db00f41c31f43
Justt-Mel <justinmelendezj@gmail.com>
2025-05-19 16:26:59 -0400

    made the app.jsx fetch the book Catalog's api data and passed the allBook variable to the Books componet to render the book data from the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from "react"
-import { Routes, Route } from "react-router-dom"
+import { useState,useEffect } from "react"
+import { Routes, Route, Link} from "react-router-dom"
 import Home from "./components/Home"
+import Books from "./components/Books"
+import axios from 'axios'
 
 function App() {
   const [token, setToken] = useState(null)
+ const [allBooks, setAllBooks] = useState([])
+
+ useEffect( ()=>{
+  const FetchBooks = async ()=>{
+    try {
+      const {data} =await axios.get('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books')
+      console.log(data)
+      setAllBooks(data)
+    } catch (error) {
+      console.error("Error fetching books:", error);
+    }
+  }
+  FetchBooks()
+},[])
 
   return (
-    <Routes>
-      <Route path ="/" element={<Home/>}/>
-    </Routes>
+    <div>
+      <div className = "NavBar">
+        <Link to ={'/'}> Home</Link>
+        <Link to ={'/Books'}> Books</Link>
+      </div>
+        <Routes>
+          <Route path ="/" element={<Home/>}/>
+          <Route path = "/Books" element = {<Books setAllBooks={setAllBooks} allBooks={allBooks} />}/>
+        </Routes>
+    </div>
   )
 }
 
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,1 +1,25 @@
-/* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
+/* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. 
+Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
+
+function Books({allBooks}) {
+    return (
+        <div>
+            <h1>Book inventory</h1>
+            <div className ="BookContainer">
+                {
+                    allBooks.map((book) => {
+                        return(
+                          < div key={book.id} className="BookCard">
+                              <h2>{book.title}</h2>
+                              <img src={book.coverimage} alt={book.name} />
+                              <h3>{book.author}</h3>
+                              <p>{book.description}</p>
+                          </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+export default Books
